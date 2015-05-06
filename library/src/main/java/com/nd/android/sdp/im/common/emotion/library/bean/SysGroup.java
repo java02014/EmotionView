be70ded5c5de451105eee2de72d137e6a5f21bcf
MrYoung91 <young91@vip.qq.com>
2015-05-06 10:02:17 +0800

    Delete unused dependency

--- library/src/main/java/com/nd/android/sdp/im/common/emotion/library/bean/SysGroup.java
+++ library/src/main/java/com/nd/android/sdp/im/common/emotion/library/bean/SysGroup.java
@@ -1,15 +1,6 @@
 package com.nd.android.sdp.im.common.emotion.library.bean;
 
-import android.content.Context;
-import android.support.v7.widget.GridLayout;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-
-import com.nd.android.sdp.im.common.emotion.library.R;
 import com.nd.android.sdp.im.common.emotion.library.stragedy.files.IFileStragedy;
-import com.nostra13.universalimageloader.core.ImageLoader;
 
 /**
  * Created by Young on 2015/4/23.
